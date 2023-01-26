713324bc4a1645327b1b441f617364f5b1a5877e
Aleksandra Szczur <olaszczur93@gmail.com>
2023-01-26 11:20:59 +0100

    fail/form challenge

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,49 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../common/models/course';
+
+const emptyCourse: Course = {
+  id: null,
+  title: '',
+  description: '',
+  percentComplete: 0,
+  favorite: false,
+};
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
   courses = [
     {
-      id: 1,
+      id: '1',
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
+      favorite: true,
     },
     {
-      id: 2,
+      id: '2',
       title: 'JavaScript the HARDEST PARTS EVER!',
       description: 'Learn JavaScript like a pro! with Will',
       percentComplete: 15,
-      favorite: true
+      favorite: true,
     },
     {
-      id: 3,
+      id: '3',
       title: 'Python Basics',
       description: 'Discover a love for Python with this course.',
       percentComplete: 30,
-      favorite: true
+      favorite: true,
     },
     {
-      id: 4,
+      id: '4',
       title: 'Java Expert',
       description: 'Become an expert in Java with Jacob',
       percentComplete: 16,
-      favorite: true
-    }
+      favorite: true,
+    },
   ];
-  selectedCourse = null;
 
-  constructor() { }
+  selectedCourse = emptyCourse;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
-  selectCourse(course: unknown) {
+  ngOnInit(): void {}
+
+  selectCourse(course: Course) {
     this.selectedCourse = course;
   }
 
@@ -51,8 +60,11 @@ export class CoursesComponent implements OnInit {
     console.log('delete course', courseId);
   }
 
-  form() {
-    console.log('delete');
+  reset() {
+    this.selectCourse({ ...emptyCourse });
   }
 
+  updateFavoriteChange() {
+    this.selectCourse;
+  }
 }
